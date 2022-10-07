28ffff0b65e38fe6ebccb6466c438a11d333e965
sepetchi <isepetci11@gmail.com>
2022-10-07 23:26:32 +0300

    cucumber step definitions

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -4,7 +4,11 @@ package com.cydeo.step_definitions;
 In the class we will be able to pass pre- & -post conditions to each scenario and each step
  */
 
+
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 
 import java.beans.BeanProperty;
 
@@ -13,6 +17,20 @@ public class Hooks {
     @Before
     public void setupScenario(){
         System.out.println("=====Setting up browser using cucumber before");
+    }
+    @After
+    public void teardownScenario(){
+        System.out.println("==== Closing browser using cucumber @After");
+        System.out.println("==== Scenario ended/ Take screenshot if failed!");
 
+    }
+    @BeforeStep
+    public void setupStep(){
+        System.out.println("------------applying setup using @BeforeStep");
+
+    }
+    @AfterStep
+    public void afterStep(){
+        System.out.println("----------> applying tearDown using @AfterStep");
     }
 }
