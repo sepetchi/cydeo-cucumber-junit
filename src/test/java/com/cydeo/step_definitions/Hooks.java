9eba1ebb011511e5e233f4bd44057a935204e87c
sepetchi <isepetci11@gmail.com>
2022-10-11 22:32:01 +0300

    take screenshot

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -5,31 +5,38 @@ In the class we will be able to pass pre- & -post conditions to each scenario an
  */
 
 
+import com.cydeo.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeStep;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.beans.BeanProperty;
 
-public class Hooks {
+public class Hooks<order> {
     //import from io.cucumber.java not from junit
-    @Before
+    @Before(order = 1)
     public void setupScenario(){
         System.out.println("=====Setting up browser using cucumber before");
     }
-    @Before("@login")
+    @Before(value = "@login", order = 2)
     public void setupScenarioForLogins(){
         System.out.println("====this will only apply for scenarios with @login tag");
     }
-    @Before("@db")
+    @Before(value = "@db", order = 0)
     public void setupforDatabaseScenarioForLogins(){
         System.out.println("====this will only apply for scenarios with @db tag");
     }
     @After
     public void teardownScenario(){
-        System.out.println("==== Closing browser using cucumber @After");
-        System.out.println("==== Scenario ended/ Take screenshot if failed!");
+        byte[] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+
+
+        Driver.closeDriver();
+        //System.out.println("==== Closing browser using cucumber @After");
+       // System.out.println("==== Scenario ended/ Take screenshot if failed!");
 
     }
     @BeforeStep
